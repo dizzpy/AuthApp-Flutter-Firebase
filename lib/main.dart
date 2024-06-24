3b428761b35951042b2c5cc1564dc94187f7dd9c
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-24 15:33:14 +0530

    reg page create

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:firebase_auth/pages/login_page.dart';
+// import 'package:firebase_auth/pages/login_page.dart';
+import 'package:firebase_auth/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: RegisterPage(),
     );
   }
 }
