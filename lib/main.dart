50823092b0d65c3e32b534b07b4994a8b3006444
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-06-25 21:00:19 +0530

    ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-// import 'package:firebase_auth/pages/login_page.dart';
 import 'package:firebase_auth/pages/home_register.dart';
 import 'package:firebase_auth/pages/login_page.dart';
 import 'package:firebase_auth/pages/register_page.dart';
-// import 'package:firebase_auth/pages/login_page.dart';
-// import 'package:firebase_auth/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
